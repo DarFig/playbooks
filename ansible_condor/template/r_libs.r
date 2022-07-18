96d370c53db2283e1e3a4bc05e77895e104a0a5c
log.Dariel <log.Dariel@gmail.com>
2022-07-18 12:15:36 +0200

    Update r_libs.r

--- ansible_condor/template/r_libs.r
+++ ansible_condor/template/r_libs.r
@@ -1,4 +1,4 @@
-load.lib<-c("tidyr","tidymodels","tidyverse","vip","visNetwork","writexl","xtable","yardstick", "summarytools","AdhereR","MLmetrics","ISLR","skimr","frequency","ggpubr")
+load.lib<-c("tidyr","tidymodels","tidyverse","vip","visNetwork","writexl","xtable","yardstick", "summarytools","AdhereR","MLmetrics","ISLR","skimr","frequency","ggpubr","themis")
 install.lib <- load.lib[!load.lib %in% installed.packages()]
 for(lib in install.lib) install.packages(lib,dependencies=TRUE)
 sapply(load.lib,require,character=TRUE)
